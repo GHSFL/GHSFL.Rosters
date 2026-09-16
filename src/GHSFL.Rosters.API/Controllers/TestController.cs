13362fdbfc67707086a17313954638c710a9d18d
jamesw98 <xeekatar@gmail.com>
2026-09-16 16:00:30 -0400

    added test endpoint

---
+++ src/GHSFL.Rosters.API/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+
+[Route("api/test")]
+public class TestController : ControllerBase
+{
+    [Route("hello-world")]
+    public ActionResult Test()
+    {
+        return Ok($"Hello world! {DateTime.UtcNow}");
+    }
+}
